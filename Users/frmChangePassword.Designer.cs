baa25741b8bfa5cda3108ba703f6f729a9ef4fd7
carlos_costa <carloscosta.pen@gmail.com>
2026-06-03 03:45:05 -0300

    Adding Change Password Form, Validation, And Functionality

---
+++ Users/frmChangePassword.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Driving_System.Users
+{
+    partial class frmChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.crtlUserCard1 = new Driving_System.Users.crtlUserCard();
+            this.tbNewPassword = new System.Windows.Forms.TextBox();
+            this.tbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.tbCurrentPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // crtlUserCard1
+            // 
+            this.crtlUserCard1.Location = new System.Drawing.Point(12, 12);
+            this.crtlUserCard1.Name = "crtlUserCard1";
+            this.crtlUserCard1.Size = new System.Drawing.Size(964, 426);
+            this.crtlUserCard1.TabIndex = 0;
+            // 
+            // tbNewPassword
+            // 
+            this.tbNewPassword.Location = new System.Drawing.Point(664, 449);
+            this.tbNewPassword.Name = "tbNewPassword";
+            this.tbNewPassword.Size = new System.Drawing.Size(187, 22);
+            this.tbNewPassword.TabIndex = 21;
+            this.tbNewPassword.Validating += new System.ComponentModel.CancelEventHandler(this.tbPassword_Validating);
+            // 
+            // tbConfirmPassword
+            // 
+            this.tbConfirmPassword.Location = new System.Drawing.Point(245, 506);
+            this.tbConfirmPassword.Name = "tbConfirmPassword";
+            this.tbConfirmPassword.Size = new System.Drawing.Size(187, 22);
+            this.tbConfirmPassword.TabIndex = 20;
+            this.tbConfirmPassword.Validating += new System.ComponentModel.CancelEventHandler(this.tbConfirmPassword_Validating);
+            // 
+            // tbCurrentPassword
+            // 
+            this.tbCurrentPassword.Location = new System.Drawing.Point(245, 445);
+            this.tbCurrentPassword.Name = "tbCurrentPassword";
+            this.tbCurrentPassword.Size = new System.Drawing.Size(187, 22);
+            this.tbCurrentPassword.TabIndex = 19;
+            this.tbCurrentPassword.Validating += new System.ComponentModel.CancelEventHandler(this.tbCurrentPassword_Validating);
+            // 
+            // label4
+            // 
+            this.label4.Image = global::Driving_System.Properties.Resources.password;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(39, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 28);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Confirm Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Image = global::Driving_System.Properties.Resources.password;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(458, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 30);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "New Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Image = global::Driving_System.Properties.Resources.download__3_;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(39, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 30);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Current Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Image = global::Driving_System.Properties.Resources.close;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(335, 583);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(97, 34);
+            this.btnClose.TabIndex = 22;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Image = global::Driving_System.Properties.Resources.Save_32;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(474, 583);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 34);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave.Validating += new System.ComponentModel.CancelEventHandler(this.btnSave_Validating);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 629);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbNewPassword);
+            this.Controls.Add(this.tbConfirmPassword);
+            this.Controls.Add(this.tbCurrentPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.crtlUserCard1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmChangePassword";
+            this.Text = "ChangePassword";
+            this.Load += new System.EventHandler(this.frmChangePassword_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private crtlUserCard crtlUserCard1;
+        private System.Windows.Forms.TextBox tbNewPassword;
+        private System.Windows.Forms.TextBox tbConfirmPassword;
+        private System.Windows.Forms.TextBox tbCurrentPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Users/frmChangePassword.cs
@@ -0,0 +1,131 @@
+﻿using BusinessLayer;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Driving_System.Users
+{
+    public partial class frmChangePassword : Form
+    {
+        private int _UserID;
+        private clsUserBusiness _User;
+
+        public frmChangePassword(int UserID)
+        {
+            InitializeComponent();
+            _UserID = UserID;
+        }
+        private void _RestValues()
+        {
+
+            tbNewPassword.Text = "";
+            tbCurrentPassword.Text = "";
+            tbConfirmPassword.Text = "";
+            tbCurrentPassword.Focus();
+
+        }
+
+        private void frmChangePassword_Load(object sender, EventArgs e)
+        {
+            _RestValues();
+            _User = clsUserBusiness.FindByUserID(_UserID);
+
+            if (_User == null)
+            {
+                MessageBox.Show("Couldn't find user", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+            crtlUserCard1.LoadUserInfo(_UserID);
+
+        }
+
+        private void tbCurrentPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbCurrentPassword.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbCurrentPassword, "Password can't be empty");
+                return;
+
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(tbCurrentPassword, null);
+            }
+            if (_User.Password != tbCurrentPassword.Text.Trim())
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbCurrentPassword, "Curretn password is't correct");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(tbCurrentPassword, null);
+            }
+
+        }
+
+        private void tbPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbNewPassword.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbNewPassword, "Password can't be empty");
+
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(tbNewPassword, null);
+            }
+        }
+
+        private void tbConfirmPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (tbConfirmPassword.Text.Trim() != tbNewPassword.Text.Trim())
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(tbConfirmPassword, "Confirm password don't match");
+
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(tbConfirmPassword, null);
+            }
+        }
+
+        private void btnSave_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                MessageBox.Show("Some field aren't valid, check refrence error icon", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _User.Password = tbNewPassword.Text.Trim();
+
+            if (_User.Save())
+            {
+                MessageBox.Show("User info Successfully saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                _RestValues();
+            }
+            else
+            {
+                MessageBox.Show("User info not saved", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

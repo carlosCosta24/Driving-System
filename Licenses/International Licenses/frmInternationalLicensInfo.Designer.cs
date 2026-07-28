e57bc21b14763b2ced09cc3b63d746df0a669c52
carlos_costa <carloscosta.pen@gmail.com>
2026-07-28 10:34:27 -0300

    International License info form

---
+++ Licenses/International Licenses/frmInternationalLicensInfo.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Driving_System.Licenses.International_Licenses
+{
+    partial class frmInternationalLicensInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ctrlInternationalLicenseInfo1 = new Driving_System.Licenses.International_Licenses.Controls.ctrlInternationalLicenseInfo();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(262, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "International Licens Info";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Driving_System.Properties.Resources.LicenseView_400;
+            this.pictureBox1.Location = new System.Drawing.Point(324, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ctrlInternationalLicenseInfo1
+            // 
+            this.ctrlInternationalLicenseInfo1.Location = new System.Drawing.Point(12, 182);
+            this.ctrlInternationalLicenseInfo1.Name = "ctrlInternationalLicenseInfo1";
+            this.ctrlInternationalLicenseInfo1.Size = new System.Drawing.Size(820, 281);
+            this.ctrlInternationalLicenseInfo1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Driving_System.Properties.Resources.close;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(737, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Close";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmInternationalLicensInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(844, 537);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ctrlInternationalLicenseInfo1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmInternationalLicensInfo";
+            this.Text = "International Licens Info";
+            this.Load += new System.EventHandler(this.frmInternationalLicensInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private Controls.ctrlInternationalLicenseInfo ctrlInternationalLicenseInfo1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Licenses/International Licenses/frmInternationalLicensInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driving_System.Licenses.International_Licenses
+{
+    public partial class frmInternationalLicensInfo : Form
+    {
+        private int _LicenseID = -1;
+        public frmInternationalLicensInfo(int InternationalLicenseID)
+        {
+            InitializeComponent();
+            _LicenseID = InternationalLicenseID;
+        }
+
+        private void frmInternationalLicensInfo_Load(object sender, EventArgs e)
+        {
+            ctrlInternationalLicenseInfo1.LoadInfo(_LicenseID);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
